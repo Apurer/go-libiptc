4b13ad9b082034939c3018f93e932d9826431e8f
Maciej <maciej@contact.apurer.io>
2021-02-14 06:31:29 +0100

    added unfinished example

--- examples/append-rule/append-rule.go
+++ examples/append-rule/append-rule.go
@@ -9,113 +9,21 @@ import (
 	"github.com/apurer/go-libiptc/libip6tc"
 )
 
-func showSyntax() {
-	fmt.Fprintf(os.Stderr, "Usage: dump-table-rules [-4|-6] <table-name>\n")
-	os.Exit(1)
-}
 
 func main() {
-	if len(os.Args) != 3 {
-		showSyntax()
-	}
-
-	var useIpv6 bool
-	switch os.Args[1] {
-	case `-4`:
-	case `-6`:
-		useIpv6 = true
-	default:
-		showSyntax()
-	}
-
-	acquired, err := common.XtablesLock(false, 0)
+	h, err := TableInit("filter")
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "append-rule: %s\n", err)
+		os.Exit(3)
 	}
-	if !acquired {
-		fmt.Fprintf(os.Stderr, "dump-table-rules: could not acquire xtables lock!\n")
-		os.Exit(1)
-	}
-	defer func() {
-		_, err := common.XtablesUnlock()
-		if err != nil {
-			panic(err)
-		}
-	}()
 
-	tableName := os.Args[2]
+	var a C.uint
+	
 
-	if !useIpv6 {
-		table, err := libip4tc.TableInit(tableName)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "dump-table-rules: %s\n", err)
-			os.Exit(3)
-		}
-		defer table.Free()
+	entry := new(IptEntry)
+	entry.ip.src.s_addr = 
 
-		// traverse trough chains
-		chain, err := table.FirstChain()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "dump-table-rules: %s\n", err)
-			os.Exit(3)
-		}
-		for chain != "" {
-			// use Go-native rules conversion
-			e, err := table.FirstRule(chain)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dump-table-rules: %s\n", err)
-				os.Exit(3)
-			}
-			for !e.IsEmpty() {
-				fmt.Println(chain+":", table.IptEntry2Rule(&e).String())
-				e, err = table.NextRule(e)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "dump-table-rules: %s\n", err)
-					os.Exit(3)
-				}
-			}
+	var c common.XtChainLabel = "INPUT"
 
-			chain, err = table.NextChain()
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dump-table-rules: %s\n", err)
-				os.Exit(3)
-			}
-		}
-	} else {
-		table, err := libip6tc.TableInit(tableName)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "dump-table-rules: %s\n", err)
-			os.Exit(3)
-		}
-		defer table.Free()
-
-		// traverse trough chains
-		chain, err := table.FirstChain()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "dump-table-rules: %s\n", err)
-			os.Exit(3)
-		}
-		for chain != "" {
-			// use Go-native rules conversion
-			e, err := table.FirstRule(chain)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dump-table-rules: %s\n", err)
-				os.Exit(3)
-			}
-			for !e.IsEmpty() {
-				fmt.Println(chain+":", table.IptEntry2Rule(&e).String())
-				e, err = table.NextRule(e)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "dump-table-rules: %s\n", err)
-					os.Exit(3)
-				}
-			}
-
-			chain, err = table.NextChain()
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dump-table-rules: %s\n", err)
-				os.Exit(3)
-			}
-		}
-	}
+	h.AppendEntry(c,)
 }
